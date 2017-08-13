32ab8546c47c5dab7b80084cc730d0694c9c9e62
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-08-13 18:46:58 -0300

    Conta quantidade de bolhas

--- ContaBolhas/main.cpp
+++ ContaBolhas/main.cpp
@@ -9,6 +9,7 @@ int main(int, char**){
     Mat image;
     stack<CvPoint> pilha;
     CvPoint coordenada, aux;
+    int cont = 51;
 
     image = imread("bolhas.png",CV_LOAD_IMAGE_GRAYSCALE);
     //imshow("image", image);
@@ -19,6 +20,7 @@ int main(int, char**){
     namedWindow("janela",WINDOW_AUTOSIZE);
     imshow("janela", image);
     waitKey();
+
     for(int i=0;i<256;i++){
         //verificando bolhas nas bordas
         if(image.at<uchar>(i,0)==255 || image.at<uchar>(i,255)==255 || image.at<uchar>(0,i)==255 || image.at<uchar>(255,i)==255){
@@ -71,7 +73,56 @@ int main(int, char**){
             imshow("janela", image);
         }
     }
-    cout<<"FIM!"<<endl;
+
+    for(int l=0;l<256;l++){
+        for(int c=0;c<256;c++){
+            if(image.at<uchar>(l,c)==255){
+                coordenada.x = l;
+                coordenada.y = c;
+
+                pilha.push(coordenada);
+
+                while(!pilha.empty()){
+                    coordenada = pilha.top();
+                    pilha.pop();
+
+                    if(image.at<uchar>(coordenada.x,coordenada.y)==255){
+                        //vizinho de esquerda
+                        if(coordenada.y-1>=0 && image.at<uchar>(coordenada.x,coordenada.y-1)==255){
+                            aux.x = coordenada.x;
+                            aux.y = coordenada.y-1;
+                            pilha.push(aux);
+                        }
+                        //vizinho da direita
+                        if(coordenada.y+1<=255 && image.at<uchar>(coordenada.x,coordenada.y+1)==255){
+                            aux.x = coordenada.x;
+                            aux.y = coordenada.y+1;
+                            pilha.push(aux);
+                        }
+                        //vizinho de baixo
+                        if(coordenada.x+1<=255 && image.at<uchar>(coordenada.x+1,coordenada.y)==255){
+                            aux.x = coordenada.x+1;
+                            aux.y = coordenada.y;
+                            pilha.push(aux);
+                        }
+                        //vizinho de cima
+                        if(coordenada.x-1>=0 && image.at<uchar>(coordenada.x-1,coordenada.y)==255){
+                            aux.x = coordenada.x-1;
+                            aux.y = coordenada.y;
+                            pilha.push(aux);
+                        }
+                        image.at<uchar>(coordenada.x,coordenada.y) = cont;
+                    }
+                }//fim do while
+                cont = cont + 1;
+                cout<<"Aperte uma tecla!"<<endl;
+                waitKey();
+                imshow("janela", image);
+            }
+        }
+    }
+
+    cout<<"FIM!"<<endl<<"Tem "<<cont-51<<" bolhas."<<endl;
     waitKey();
 
     return 0;
