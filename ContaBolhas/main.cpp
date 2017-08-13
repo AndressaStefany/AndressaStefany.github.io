f0bbb9ef67fb6fc9ecdeb066392462586abc8895
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-08-13 19:44:18 -0300

    conta buracos

--- ContaBolhas/main.cpp
+++ ContaBolhas/main.cpp
@@ -5,34 +5,34 @@
 using namespace cv;
 using namespace std;
 
-void myseedfill(Mat image, int cor, stack<CvPoint> pilha){
+void myseedfill(Mat image, int cor, int searchcor, stack<CvPoint> pilha){
     CvPoint coordenada, aux;
 
     while(!pilha.empty()){
         coordenada = pilha.top();
         pilha.pop();
 
-        if(image.at<uchar>(coordenada.x,coordenada.y)==255){
+        if(image.at<uchar>(coordenada.x,coordenada.y)==searchcor){
             //vizinho de esquerda
-            if(coordenada.y-1>=0 && image.at<uchar>(coordenada.x,coordenada.y-1)==255){
+            if(coordenada.y-1>=0 && image.at<uchar>(coordenada.x,coordenada.y-1)==searchcor){
                 aux.x=coordenada.x;
                 aux.y=coordenada.y-1;
                 pilha.push(aux);
             }
             //vizinho da direita
-            if(coordenada.y+1<=255 && image.at<uchar>(coordenada.x,coordenada.y+1)==255){
+            if(coordenada.y+1<=255 && image.at<uchar>(coordenada.x,coordenada.y+1)==searchcor){
                 aux.x=coordenada.x;
                 aux.y=coordenada.y+1;
                 pilha.push(aux);
             }
             //vizinho de baixo
-            if(coordenada.x+1<=255 && image.at<uchar>(coordenada.x+1,coordenada.y)==255){
+            if(coordenada.x+1<=255 && image.at<uchar>(coordenada.x+1,coordenada.y)==searchcor){
                 aux.x=coordenada.x+1;
                 aux.y=coordenada.y;
                 pilha.push(aux);
             }
             //vizinho de cima
-            if(coordenada.x-1>=0 && image.at<uchar>(coordenada.x-1,coordenada.y)==255){
+            if(coordenada.x-1>=0 && image.at<uchar>(coordenada.x-1,coordenada.y)==searchcor){
                 aux.x=coordenada.x-1;
                 aux.y=coordenada.y;
                 pilha.push(aux);
@@ -40,16 +40,13 @@ void myseedfill(Mat image, int cor, stack<CvPoint> pilha){
             image.at<uchar>(coordenada.x,coordenada.y) = cor;
         }
     }//fim do while
-    cout<<"Aperte uma tecla!"<<endl;
-    waitKey();
-    imshow("janela", image);
 }
 
 int main(int, char**){
     Mat image;
     stack<CvPoint> pilha;
     CvPoint coordenada;
-    int cont = 51;
+    int cont = 51, buracos = 1, total;
 
     image = imread("bolhas.png",CV_LOAD_IMAGE_GRAYSCALE);
     //imshow("image", image);
@@ -59,7 +56,6 @@ int main(int, char**){
 
     namedWindow("janela",WINDOW_AUTOSIZE);
     imshow("janela", image);
-    waitKey();
 
     //verificando bolhas nas bordas
     for(int i=0;i<256;i++){
@@ -76,8 +72,7 @@ int main(int, char**){
 
             pilha.push(coordenada);
 
-            myseedfill(image, 0, pilha);
-
+            myseedfill(image, 0, 255, pilha);
             cout<<"Aperte uma tecla!"<<endl;
             waitKey();
             imshow("janela", image);
@@ -93,7 +88,7 @@ int main(int, char**){
 
                 pilha.push(coordenada);
 
-                myseedfill(image, cont, pilha);
+                myseedfill(image, cont, 255, pilha);
 
                 cont = cont + 1;
                 cout<<"Aperte uma tecla!"<<endl;
@@ -103,11 +98,34 @@ int main(int, char**){
         }
     }
 
+    total = cont-51;
+
     //pinta o fundo
+    coordenada.x = 0;
+    coordenada.y = 0;
+
+    pilha.push(coordenada);
+
+    myseedfill(image, 255, 0, pilha);
 
     //conta as bolhas com buraco
+    for(int l=0;l<256;l++){
+        for(int c=0;c<256;c++){
+            if(image.at<uchar>(l,c)==0){
+                coordenada.x = l;
+                coordenada.y = c;
 
-    cout<<"FIM!"<<endl<<"Tem "<<cont-51<<" bolhas."<<endl;
+                pilha.push(coordenada);
+
+                myseedfill(image, buracos, 0, pilha);
+
+                buracos++;
+            }
+        }
+    }
+
+
+    cout<<endl<<endl<<"Tem "<<buracos-1<<" bolhas com buracos e "<<total-buracos<<" bolhas sem buracos."<<endl<<endl<<"FIM!"<<endl;
     waitKey();
 
     return 0;
