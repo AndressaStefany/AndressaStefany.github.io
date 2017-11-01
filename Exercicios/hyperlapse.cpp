7471a1964bf99175504d39862d75336f6dacc2d8
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-10-31 22:56:04 -0300

    hyperlapse nao acabado

--- Exercicios/hyperlapse.cpp
+++ Exercicios/hyperlapse.cpp
@@ -8,10 +8,41 @@
 using namespace cv;
 using namespace std;
 
+float distancia(CvPoint pt1, CvPoint pt2) {
+    int dx = pt2.x - pt1.x;
+    int dy = pt2.y - pt1.y;
+    return cvSqrt( (float)(dx*dx + dy*dy));
+}
+
+CvPoint deslocamento(CvPoint pt1, CvPoint pt2) {
+    CvPoint dR;
+    dR.x = (int) pt1.x - pt2.x;
+    dR.y = (int) pt1.y - pt2.y;
+
+    return dR;
+}
+
+CvPoint soma(CvPoint pt1, CvPoint pt2) {
+    CvPoint dR;
+    dR.x = (int) pt1.x + pt2.x;
+    dR.y = (int) pt1.y + pt2.y;
+
+    return dR;
+}
+
+CvPoint media(CvPoint pt, int tam) {
+    CvPoint dR;
+    dR.x = (int) pt.x / tam;
+    dR.y = (int) pt.y / tam;
+
+    return dR;
+}
+
 int main(int argc, char** argv){
     char key;
-    int cont=0,normal[2];
-    double sumX, sumY;
+    int cont=0;
+    CvPoint desl, mediaDesl;
+    Rect teste;
 
     Mat prev_image, next_image, image, mask, image_cut;
     vector<Point2f> corners[2];
@@ -31,11 +62,6 @@ int main(int argc, char** argv){
     resize(prev_image,prev_image,Size(768,432));
     image = prev_image.clone();
     cvtColor(prev_image, prev_image, CV_BGR2GRAY);
-
-    //nova imagem
-//    image_cut = next_image.clone();
-//    resize(image_cut,image_cut,Size(758,422));
-//    cvtColor(image_cut, image_cut, CV_BGR2GRAY);
 
     goodFeaturesToTrack(prev_image, corners[0], 50, 0.01, 20, mask, 3, false, 0.04);
 
@@ -58,32 +84,30 @@ int main(int argc, char** argv){
         resize(next_image,next_image,Size(768,432));
         image = next_image.clone();
         cvtColor(next_image, next_image, CV_BGR2GRAY);
+        //nova imagem
+        image_cut = next_image.clone();
 
         calcOpticalFlowPyrLK(prev_image, next_image, corners[0], corners[1], status, err, winSize,3,
                              termcrit,0, 0.001);
 
-        sumX = 0;
-        sumY = 0;
-//        circle(image, Point2f(0,0), 3, Scalar(0, 255, 0), -1, 8);
-//        cout<<corners[0].size()<<"    "<<corners[1].size()<<endl;
-        for (size_t i = 0; i < corners[1].size(); i++) {
-            //media da distancia entre os pontos
-            sumX += (abs(corners[0][i].x-corners[1][i].x)/2);
-            sumY += (abs(corners[0][i].y-corners[1][i].y)/2);
+        desl.x = 0;
+        desl.y = 0;
 
+        for (size_t i = 0; i < corners[1].size(); i++) {
+            //soma dos deslocamentos entre os pontos
+            desl = soma(deslocamento(corners[0][i],corners[1][i]), desl);
             // pinta os pontos em image
             circle(image, corners[1][i], 3, cor, -1, 8);
             // atualiza os pontos
             corners[0][i] = corners[1][i];
-
         }
-        //verificar os pontos perdidos (?)
-        normal[0] = (int) sumX/corners[0].size();
-        normal[1] = (int) sumY/corners[0].size();
+        //media do deslocamento
+        mediaDesl = media(desl, corners[1].size());
 
-//        cout<<normal[0]<<" , "<<normal[1]<<endl;
+        //ERROR AQUI
+        teste =  Rect(corners[1][0].x - mediaDesl.x, corners[1][0].y - mediaDesl.y, 668,332);
 
-        corners[1].resize(0);
+        image_cut = image_cut(teste);
 
         imshow("window", image);
 
